eeb7dd355fe6a1e52d8a2577b4a2126587251388
prtkm <prateekm@cmu.edu>
2016-08-29 21:56:33 -0400

    update runvasp.py

--- bin/runvasp.py
+++ bin/runvasp.py
@@ -20,6 +20,24 @@ if 'PBS_NODEFILE' in os.environ:
         parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
         print('Running "{}"'.format(parcmd))
         exitcode = os.system(parcmd)
+
+elif 'PE_HOSTFILE' in os.environ:
+    # we are in the queue. determine if we should run serial or parallel
+    NPROCS = int(os.environ['NSLOTS'])
+    NODES = int(os.environ['NHOSTS'])
+
+    if NPROCS == 1:
+        # no question. running in serial.
+        exitcode = os.system(serial_vasp)
+
+    else:
+        # We are running some kind of parallel job. This script only
+        # supports MPI. It used to support multiprocessing, but it was
+        # confusing, so I have taken it out for now.
+        parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
+        print('Running "{}"'.format(parcmd))
+        exitcode = os.system(parcmd)        
+
 else:
     # probably running at cmd line, in serial.
     exitcode = os.system(serial_vasp)
